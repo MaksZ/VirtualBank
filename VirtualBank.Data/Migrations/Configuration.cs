6990e8dbaed39a70ac638985ce3841121ce03e95
Maksim Zavidejev <maksim.zavidejev@gmail.com>
2019-04-07 11:54:33 +0300

    Support of EF added

---
+++ VirtualBank.Data/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace VirtualBank.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<VirtualBankContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(VirtualBankContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ VirtualBank.Data/VirtualBankContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualBank.Data
+{
+    public class VirtualBankContext : DbContext
+    {
+        public VirtualBankContext() : base("name=VirtualBankDb")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
